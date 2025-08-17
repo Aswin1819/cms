78a3dc300e4e5d90fcdb281bebe61dba34c5d314
Aswin1819 <aswinachumathra@gmail.com>
2025-08-17 12:03:52 +0530

    Updation:Landing url Created

--- ivy_verse/urls.py
+++ ivy_verse/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,6 +26,7 @@ urlpatterns = [
     path('api/admin/', include('adminapp.urls')),
     path('api/blogs/', include('blogs.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('', RedirectView.as_view(url='/api/auth/login/', permanent=False), name='home'),
     
 ]
 
